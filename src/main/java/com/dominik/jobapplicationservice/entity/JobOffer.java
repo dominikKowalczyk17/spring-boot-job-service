e34abc26668d29090258eb1cc60d8034bca389e8
Dominik Kowalczyk <kowalczyk.dominik17@gmail.com>
2025-08-29 10:49:08 +0200

    init commit

---
+++ src/main/java/com/dominik/jobapplicationservice/entity/JobOffer.java
@@ -0,0 +1,32 @@
+package com.dominik.jobapplicationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "job_offers")
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobOffer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String title;
+
+    @Column(nullable = false)
+    String salaryRange;
+
+    @Column(nullable = false)
+    String company;
+
+    @Column(unique = true, nullable = false, length = 1000)
+    String url;
+
+    @Column(nullable = false)
+    Boolean applied;
+}
